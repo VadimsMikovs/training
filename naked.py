b67a5efb4e9330d8427bb66e76b5ad7439f1925b
VM <jurisjuris545@gmail.com>
2022-01-03 21:54:47 +0000

    pievientoi faili no parauga

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "RgUTPacsFnnUfcD42tHOoLh5eCJ7hvnEsT9bDoI6"
+nasa_api_key = "???"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
